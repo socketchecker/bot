106b97221ce0c90660d2aa2d1e8d7944c6ef40b6
PowerOfMad <mantas7778@gmail.com>
2017-09-17 20:10:35 +0300

    Update bottest2.js

--- bottest2.js
+++ bottest2.js
@@ -1808,7 +1808,7 @@
 
             stoprouletteCommand: {
                 command: 'stoproulette', // The command to be called. With the standard command literal this would be: !bacon
-                rank: 'cohostr', // Minimum user permission to use the command
+                rank: 'cohost', // Minimum user permission to use the command
                 type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
                 functionality: function(chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length)

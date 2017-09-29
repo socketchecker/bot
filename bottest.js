be71bcf7b39cea13fa36a766d432cb160a655339
PowerOfMad <mantas7778@gmail.com>
2017-09-29 22:11:58 +0300

    Update bottest.js

--- bottest.js
+++ bottest.js
@@ -2687,13 +2687,10 @@
                 type: 'exact',
                 functionality: function(chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void(0);
-                    else 
-                        var pos = API.getWaitListPosition(user.id);
-                        if (pos < 0) { return API.sendChat(subChat(basicBot.chat.notinwaitlist, {
-                            name: name
-                        }));    
-                   else { (basicBot.room.roulette.rouletteStatus && basicBot.room.roulette.participants.indexOf(chat.uid) < 0) {
+                    if (!basicBot.commands.executable(this.rank, chat)) return void(0); 
+                   else { 
+                       var pos = API.getWaitListPosition(user.id);
+                       (basicBot.room.roulette.rouletteStatus && basicBot.room.roulette.participants.indexOf(chat.uid) < 0) && (pos < 0) {
                             basicBot.room.roulette.participants.push(chat.uid);
                             API.sendChat(subChat(basicBot.chat.roulettejoin, {
                                 name: chat.un
@@ -2701,9 +2698,8 @@
                         }
                     }
                 }
-            }
-         }
-         },
+            },
+        
 
             jointimeCommand: {
                 command: 'jointime',
